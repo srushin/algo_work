fdbedcd76bf0cd9621d14f20a469622f68e7085d
Steven Rushin <78064648+srushin@users.noreply.github.com>
2024-11-18 12:09:26 -0500

    Update algo_practice_4.py
    added question 2

--- algo_practice_4.py
+++ algo_practice_4.py
@@ -16,3 +16,28 @@ def even_first(arr):
 test_arr = [7, 3, 5, 6, 4, 10, 3, 2] # should equal [6, 4, 10, 2, 7, 3, 5, 3]
 even_first(test_arr)
 print(test_arr)
+
+
+# Increment a Number
+# Write a program that takes as input a list of digits encoding a nonnegative decimal integer D and updates the list to represent the integer D + 1.
+# For example, if the input is [1, 2, 9] then you should update the list to [1, 3, 0].
+
+def plus_one(arr):
+    arr[-1] += 1
+    for i in reversed(range(1, len(arr))):
+        if arr[i] != 10:
+            break
+        arr[i] = 0
+        arr[i - 1] += 1
+
+    if arr[0] == 10:
+        arr[0] = 1
+        arr.append(0)
+
+    return arr
+
+test_num1 = [1, 2, 9]
+test_num2 = [9, 9, 9]
+
+print(plus_one(test_num1))
+print(plus_one(test_num2))
